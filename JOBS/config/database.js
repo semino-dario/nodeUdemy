3fe972cc0970263e50189aea22342507f66af5eb
Dario Semino <orbislibroscontacto@gmail.com>
2023-08-19 18:28:13 -0300

    With db changes for Cyclic connection

--- JOBS/config/database.js
+++ JOBS/config/database.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose')
 
-const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URI, {
+// const connectDatabase = () => {
+//     mongoose.connect(process.env.DB_URI, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     }).then(con => {
+//         console.log(`MongoDB Database connected with host:${con.connection.host} `)
+//     })
+// }
+
+const connectDatabase = async () => {
+
+    await mongoose.connect(process.env.DB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(con => {
         console.log(`MongoDB Database connected with host:${con.connection.host} `)
     })
 }
-
 
 module.exports = connectDatabase;
